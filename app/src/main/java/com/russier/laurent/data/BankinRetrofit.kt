d43dc234f39c3126c8c9f55ce1f2adfe1ab3ad0b
laurent russier <laurent.russier@gmail.com>
2019-04-14 01:09:10 +0200

    Add io schedulers on retrofit

--- app/src/main/java/com/russier/laurent/data/BankinRetrofit.kt
+++ app/src/main/java/com/russier/laurent/data/BankinRetrofit.kt
@@ -1,5 +1,6 @@
 package com.russier.laurent.data
 
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -7,7 +8,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 class BankinRetrofit(baseUrl: String) {
     val retrofit: Retrofit = Retrofit
         .Builder()
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
         .addConverterFactory(MoshiConverterFactory.create())
         .baseUrl(baseUrl)
         .build()
