7c335d31ee0bfa9619104fba01c6f27a943ff4bf
laurent russier <laurent.russier@gmail.com>
2019-04-14 01:05:58 +0200

    Fix retrofit vulnerabilites

--- app/src/main/java/com/russier/laurent/data/BankinRetrofit.kt
+++ app/src/main/java/com/russier/laurent/data/BankinRetrofit.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class BankinRetrofit(val baseUrl: String) {
-    val retrofit = Retrofit
+class BankinRetrofit(baseUrl: String) {
+    val retrofit: Retrofit = Retrofit
         .Builder()
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(MoshiConverterFactory.create())
